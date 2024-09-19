5ef410d307a1cb89db80181587f85cba2133fd2f
Maximiliano Paris <mparis@hotmail.cl>
2024-09-18 20:43:26 -0600

    Initial Commit

---
+++ Push-up Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Push-up Tracker
+//
+//  Created by Maximiliano París Gaete on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Push-up Tracker/Push_up_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Push_up_TrackerApp.swift
+//  Push-up Tracker
+//
+//  Created by Maximiliano París Gaete on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Push_up_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
